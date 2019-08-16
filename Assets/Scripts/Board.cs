dbf033b1eb99f2e8bd376c2bf0dbea657b6aba33
leeleelee394 <leeleelee394@gmail.com>
2019-08-16 19:45:39 +0900

    SceneAddPrefabsCandy

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -10,22 +10,70 @@ public class Board : MonoBehaviour
 
     //2. 딕셔너리를 사용 Key, value
     //딕셔너리를 사용하면 2차원 배열보다 사용하기 쉽고 가독성이 좋다.
-    private Dictionary<string, Tile> m_TileDictionary = new Dictionary<string, Tile>();
-    
-   
-    
+    private Dictionary<string, Tile> m_TilesDictionary = new Dictionary<string, Tile>();
 
+    private GameObject m_TilePrefab;
+
+    private int m_Width = 3;
+    private int m_Height = 3;    
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject tile_0 = Resources.Load("Prefabs/Candy_Purple_Bomb") as GameObject;
-        tile_0.transform.position = Vector3.zero;
+        /*
+        //GameObject tilePrefab = Resources.Load("Prefabs/Candy_Purple_Bomb") as GameObject;
+        Tile tile_0 = Instantiate<Tile>(tilePrefab.transform.GetComponent<Tile>());
+        //타일의 부모를 Board로 설정
+        tile_0.transform.SetParent(this.transform);
+        */
+
+        m_TilePrefab = Resources.Load("Prefabs/Candy_Purple_Bomb") as GameObject;
+        CreateTiles();
+        
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //데이터 입력(input or parameter) => 처리(process) => 출력(output, return)
+    
+    ///<summary>
+    ///프리팹을 이용해서 새로운 타일들을 생성한다.
+    ///</summary>
+    private void CreateTiles()
     {
-        
+        for (int y = 0; y < m_Height; ++y)
+        { 
+            for (int x = 0; x < m_Width; ++x)
+            {
+                string key = x.ToString() + "," + y.ToString();
+
+                Tile tile = Instantiate<Tile>(m_TilePrefab.transform.GetComponent<Tile>());
+
+                tile.transform.SetParent(this.transform);
+                tile.transform.position = new Vector3(x*1.2f, y*1.2f, 0f);                
+                m_TilesDictionary.Add(key, tile);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Tile을 반환한다.
+    /// </summary>
+    /// <param name="x">좌표</param>
+    /// <param name="y">좌표</param>
+    /// <returns>Tile</returns>
+    public Tile GetTile(int x, int y)
+    {
+        string key = x.ToString() +","+ y.ToString();
+        return m_TilesDictionary[key];
+    }
+
+    /// <summary>
+    /// Tile을 반환한다.
+    /// </summary>
+    /// <param name="xy">좌표</param>
+    /// <returns>Tile</returns>
+    public Tile GetTile(string xy)
+    {
+        return m_TilesDictionary[xy];
     }
 }
