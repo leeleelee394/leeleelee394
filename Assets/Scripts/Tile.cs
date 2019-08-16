6d321a21e8a33898321fe196b55e081278cbd655
leeleelee394 <leeleelee394@gmail.com>
2019-08-16 16:42:51 +0900

    AddGameManager

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Tile : MonoBehaviour
 {
+    //Properties
+    public int m_ID = 101;    
+
+    /* Methods 함수 */
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {       
     }
 
     // Update is called once per frame
@@ -15,4 +19,16 @@ public class Tile : MonoBehaviour
     {
         
     }
+
+    void OnMouseDown()
+    {
+        Vector3 strength = new Vector3(0.1f, 0.1f);
+        transform.DOPunchScale(strength, 1f);
+    }
+
+
+
+    //이동()
+    //폭발()
+    //힌트표시()
 }
